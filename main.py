9ae61d77b20c66be92292bad96f5b9d0b3f79eae
Pablo Céspedes <pablocesar.cespedes98@gmail.com>
2023-10-24 21:20:40 -0600

    Notification
    >After reading a QR it shows a notification for 10 seconds

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import cv2
+import time
+from plyer import notification
 
 def leer_qr(path_qr):
     img=cv2.imread(path_qr)
     detector=cv2.QRCodeDetector()
     data=detector.detectAndDecode(img)
-    return data
+    return data[0]
 
-print(leer_qr("QR-example.png"))
+message=leer_qr("frame.png")
+notification.notify(
+    title="This is a python test!!",
+    message=str(message),
+    timeout=10
+)
+
+time.sleep(10)
